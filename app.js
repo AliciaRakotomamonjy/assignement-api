f4598d3e1931400bbe013426efbdf5d8b10b828c
HenintsoaRatsim <ratsimhenintsoa@gmail.com>
2024-03-05 20:04:31 +0300

    commit modification token

--- app.js
+++ app.js
@@ -13,16 +13,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.json());
-
+const { verifyToken }= require('./middleware/authentification');
 const utilisateurRoute =require("./routes/utilisateurRoute");
 const authRoute =require("./routes/authRoute");
 const assignmenthRoute =require("./routes/assignmentRoute");
 
 require("./base/moongosedb");
-
 const prefix = '/api';
+app.use(prefix+"/",authRoute)
 app.use(prefix+"/auth",authRoute)
-require('./middleware/authentification')
+app.use(verifyToken);
 app.use(prefix+"/utilisateur",utilisateurRoute)
 app.use(prefix+"/assignment",assignmenthRoute)
 
--- middleware/authentification.js
+++ middleware/authentification.js
@@ -2,19 +2,29 @@ const jwt = require("jsonwebtoken");
 
 
 function verifyToken(req, res, next) {
+  try {
     const token = req.headers.authorization.split(' ')[1];
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
       if (err) {
-        console.log('err', "Token invalide");
-        return res.status(400).json({ message: 'Token invalide' });
+        return res.status(400).json({
+          message: 'Token invalide'
+        });
       }
-      req.utilisateur = decoded.id;
-      req.utilisateur = decoded.email;
-      req.utilisateur = decoded.role;
+      req.utilisateur = {
+        id: decoded.id,
+        email: decoded.email,
+        role: decoded.role
+      };
       next();
+    });
+  } catch {
+    return res.status(400).json({
+      message: 'Pas de Token'
     });
   }
 
+}
+
 module.exports = {
- verifyToken
+  verifyToken
 }
